b0d7b664f80b703a3693dc8fb18e9a46f7b715db
bogem <albertnigma@gmail.com>
2015-11-22 11:44:07 +0100

    Remove one newline in TrackManager#proccess_tracks

--- lib/vnehm/track_manager.rb
+++ lib/vnehm/track_manager.rb
@@ -12,7 +12,6 @@ module Vnehm
 
     def process_tracks(tracks)
       tracks.reverse_each do |track|
-        UI.newline
         dl(track)
         tag(track)
         @playlist.add_track(track) if @playlist
